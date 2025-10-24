85246a7f5e33aa7e90aad075b274e48d752c8712
nikaTqw <161809588+nikaTqw@users.noreply.github.com>
2025-10-24 17:50:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// offcanvas navbar
+$("#navbarOffcanvasLg a").click(function(){
+    $('.offcanvas').offcanvas('hide');
+});
+
+// tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// forms
+(() => {
+  'use strict'
+
+  const forms = document.querySelectorAll('.needs-validation')
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
